8cf00b21260bd9f0f876797d1494e10500c8ff8b
Siddarth_patel <siddarthpatel51@gmail.com>
2026-08-16 13:58:15 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pypickle
+model=pypickle.load("placement_model.pkl")
+st.title("Placement-Prediction App")
+cgpa=st.slider("CGPA",1.0,10.0,7.0)
+apt=st.slider("Aptitude Score",1,100,60)
+cs=st.slider("communication Skills",1,100,50)
+ins=st.selectbox("Internship",["Yes","No"])
+# otr=st.slider("Other Skills",1,100,50)
+project=st.number_input("Projects",0)
+submit=st.button("Predict Placement",type="primary")
+
+if submit:
+    if ins=="Yes":
+       ins=1
+    else:
+        ins=0
+    result=model.predict([[cgpa,apt,cs,ins,project]])
+    if result[0]==1:
+        st.success("student can be placed.")
+    else:
+        st.error("student can not be placed.")
+
+
