c563c8ab9b4ec5be8226eec781a59f593bf38803
Siddarth_patel <siddarthpatel51@gmail.com>
2026-08-17 23:15:28 +0530

    Create About page for Placement Prediction System
    Added information about the Placement Prediction System and parameters used.

---
+++ pages/4_About.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("ℹ️ About")
+
+st.write("""
+The Placement Prediction System is designed to help students
+understand their placement possibility based on academic performance
+and selected skill-related parameters.
+
+The system will use machine learning to generate a placement prediction.
+""")
+
+st.subheader("Parameters Used")
+
+st.write("""
+- CGPA
+- Aptitude Score
+- Communication Skills
+- Internship Experience
+- Number of Projects
+""")
