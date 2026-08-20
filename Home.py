83e5f461a99b94bed318c70b1706597f0cd52792
Siddarth_patel <siddarthpatel51@gmail.com>
2026-08-20 23:14:50 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,25 +1,14 @@
 import streamlit as st
-import pickle
-with open("placement_model.pkl", "rb") as file:
-    model = pickle.load(file)
-st.title("Placement-Prediction App")
-cgpa=st.slider("CGPA",1.0,10.0,7.0)
-apt=st.slider("Aptitude Score",1,100,60)
-cs=st.slider("communication Skills",1,100,50)
-ins=st.selectbox("Internship",["Yes","No"])
-# otr=st.slider("Other Skills",1,100,50)
-project=st.number_input("Projects",0)
-submit=st.button("Predict Placement",type="primary")
 
-if submit:
-    if ins=="Yes":
-       ins=1
-    else:
-        ins=0
-    result=model.predict([[cgpa,apt,cs,ins,project]])
-    if result[0]==1:
-        st.success("student can be placed.")
-    else:
-        st.error("student can not be placed.")
+st.set_page_config(
+    page_title="Placement Prediction System",
+    page_icon="🎓",
+    layout="wide"
+)
 
+st.title("🎓 Placement Prediction System")
 
+st.write("""
+Welcome to the Placement Prediction System.
+Use the sidebar to navigate through the application.
+""")
